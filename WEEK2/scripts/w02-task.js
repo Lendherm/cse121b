7417ff545d487751bb044d2251a326d846501e8c
Lendherm <dluisemilionathanael@gmail.com>
2024-01-15 19:09:41 -0600

    finished

---
+++ WEEK2/scripts/w02-task.js
@@ -0,0 +1,54 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+
+/* Step 2 - Variables */
+let fullName = 'Luis Emilio Nathanael De la rosa Meneses'
+let currentYear = '2024'
+let profilePicture = 'images/IMG-20230729-WA0236.jpg'
+let favoriteFoods = ['Pizza', 'Chocolate', 'Sushi'];
+let newFavoriteFood = 'Ice Cream';
+
+
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name')
+const foodElement = document.getElementById('food')
+const yearElement = document.getElementById('year')
+const imageElement = document.querySelector('img')
+
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute ('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+foodElement.textContent = favoriteFoods.join(', ');
+favoriteFoods.push(newFavoriteFood);
+foodElement.innerHTML += '<br>' + favoriteFoods.join('<br>');
+favoriteFoods.shift();
+foodElement.innerHTML += '<br>' + favoriteFoods.join('<br>');
+favoriteFoods.pop();
+foodElement.innerHTML += '<br>' + favoriteFoods.join('<br>');
+
+
+
+
+
+
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
